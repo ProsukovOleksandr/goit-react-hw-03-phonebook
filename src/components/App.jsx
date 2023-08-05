e9339f354980fbac74d6f56608fff105b11ca7a7
Prosukov Oleksandr <122283728+ProsukovOleksandr@users.noreply.github.com>
2023-08-05 09:52:15 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,8 +21,10 @@ export class App extends Component {
       })
     }
   };
-  componentDidUpdate(){
+  componentDidUpdate(prevState){
+    if(this.state.contacts!==prevState.contacts){
     localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
   }
   handleAddContact = contact => {
     const { contacts } = this.state;
